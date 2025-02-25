996556d7daf1ccdf8f2861de7bd3b11ca8b13d14
Annehz <luckylzn@gmail.com>
2025-02-25 22:51:11 +0800

    edited markdown

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # terraform_gpu_ec2_instance_aws
 
+You may refer to my blog post, [How To Launch A GPU EC2 Instance On AWS: A Step-by-Step Guide And Terraform Code](https://medium.com/@luzhenna/how-to-launch-a-gpu-ec2-instance-on-aws-a-step-by-step-guide-and-terraform-code-aec524ed925b), for a step-by-step guide on how to set up a GPU-accelerated EC2 instance on AWS.
--- variables.tf
+++ variables.tf
@@ -13,5 +13,5 @@ variable "ec2_instance_type" {
 variable "public_key" {
   description = "Public key to be used for SSH access."
   type        = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLsw5Z7Go1121Rfl2o2L3h2w1zGAi/8tJFCjgzk/Sd4FuaGMzBMdSXAn4n1NxT3We/k9HapRcJlHWcxSSUHPVuwJfNNTTuxAqWOCbroOK58MsKhPWN0NyETVIZ9K0J5azpGSAfd2hHIvwCllmP/Zab+pt1VmOf5QQIx3yGHltzBW7RvBa4O+8YMBRBdbT2znUF3uFxhjnXDod/f3VdXn5dAyc80KPesfE0tXeZxvyjHWi1REzTKH0Ae1agnX79pp09jiUBDyv+acTv8XdTfb7hZ/YqCZ8IEOMmB00xPcI+KhFvGTx2/hVlMhVfw6oEgRTm5ixYswEpRM43SmKu5A0/"
+  default = "<type your public key here>"
 }
